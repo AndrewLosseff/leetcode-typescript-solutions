26846baa1b8edbdcce63bbdabffd6db90b5e6e75
AndrewLosseff <AndrewLosseff@gmail.com>
2022-10-14 12:37:35 +0300

    add solution for Two Sum

--- src/001-TwoSum/solutions.ts
+++ src/001-TwoSum/solutions.ts
@@ -1,1 +1,17 @@
 // There are some solutions for the problem
+
+// #1 
+export function twoSum(nums: number[], target: number): number[] {
+    const hash: { [key: number]: number } = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i];
+        if (diff in hash) {
+            return [hash[diff], i];
+        } else {
+            hash[nums[i]] = i;
+        }
+    }
+
+    return []
+};
