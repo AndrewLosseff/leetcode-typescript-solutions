0b705c3c544916e87b85f609ec20793ced35bff0
AndrewLosseff <AndrewLosseff@gmail.com>
2022-10-17 11:32:56 +0300

    add solutions

--- src/704-BinarySearch/index.ts
+++ src/704-BinarySearch/index.ts
@@ -1,3 +1,3 @@
 export function search(nums: number[], target: number): number {
-   
+    
 };
--- src/704-BinarySearch/solutions.ts
+++ src/704-BinarySearch/solutions.ts
@@ -1,2 +1,24 @@
 // There are some solutions for the problem
 // #1
+export function search(nums: number[], target: number): number {
+    return nums.indexOf(target)
+};
+
+// #2
+export function search(nums: number[], target: number): number {
+    let lowPoint = 0
+    let highPoint = nums.length
+    
+    do {
+
+        const midPoint = Math.floor(lowPoint + (highPoint - lowPoint)/2)
+        const value = nums[midPoint]
+
+        if(value === target) return midPoint
+        else if(value > target) highPoint = midPoint
+        else lowPoint = midPoint + 1
+
+    } while(lowPoint < highPoint)
+
+    return -1
+};
