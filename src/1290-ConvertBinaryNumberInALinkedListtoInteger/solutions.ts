b9357113c0eecbf357d655488d0bd4f1f313cc73
AndrewLosseff <AndrewLosseff@gmail.com>
2022-10-19 10:36:29 +0300

    add solution for the problem

--- src/1290-ConvertBinaryNumberInALinkedListtoInteger/solutions.ts
+++ src/1290-ConvertBinaryNumberInALinkedListtoInteger/solutions.ts
@@ -1,2 +1,18 @@
 // There are some solutions for the problem
 // #1
+export function getDecimalValue(arr): number {
+    // Helper function to convert arr to Linkes List
+    const head: ListNode | null = arrayToList(arr)
+    // Solution to the problem
+
+
+    let current = head
+    let binary = ""
+    
+    while(current !== null) {
+        binary += current?.val
+        current = current!.next
+    }
+
+    return parseInt(binary, 2)
+};
