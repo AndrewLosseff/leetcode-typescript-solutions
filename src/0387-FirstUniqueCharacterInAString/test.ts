5dc15f1faaecd6102097902ad7225dc1c38fe913
AndrewLosseff <AndrewLosseff@gmail.com>
2022-10-20 10:24:41 +0300

    add First Unique Character in a String

---
+++ src/0387-FirstUniqueCharacterInAString/test.ts
@@ -0,0 +1,20 @@
+// To run test use this:
+// yarn jest src/387-FirstUniqueCharacterInAString --watch
+
+import { firstUniqChar } from './index'
+
+test('First Unique Character in a String', () => {
+  expect(firstUniqChar).toBeDefined();
+});
+
+test('leetcode', () => {
+    expect(firstUniqChar("leetcode")).toEqual(0)
+});
+
+test('loveleetcode', () => {
+    expect(firstUniqChar("loveleetcode")).toEqual(2)
+});
+
+test('aabb', () => {
+  expect(firstUniqChar("aabb")).toEqual(-1)
+});
