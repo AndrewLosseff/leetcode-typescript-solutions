35d006c80bac1bd1beac8eb920269862807ede0a
AndrewLosseff <AndrewLosseff@gmail.com>
2022-10-27 17:07:30 +0300

    add problem

---
+++ src/0203-RemoveLinkedListElements/test.ts
@@ -0,0 +1,20 @@
+// To run test use this:
+// yarn jest src/0203-RemoveLinkedListElements --watch
+
+import { removeElements } from './index'
+
+test('Remove Linked List Elements', () => {
+  expect(removeElements).toBeDefined();
+});
+
+test('head = [1,2,6,3,4,5,6]', () => {
+    expect(removeElements([1,2,6,3,4,5,6], 6)).toEqual([1,2,3,4,5])
+});
+
+test('head = []', () => {
+  expect(removeElements([], 1)).toEqual([])
+});
+
+test('head = [7,7,7,7]', () => {
+  expect(removeElements([7,7,7,7], 7)).toEqual([])
+});
