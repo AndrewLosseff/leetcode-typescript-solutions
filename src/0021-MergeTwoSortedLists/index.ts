bb00af21e371521cc38934eeef31d282c5c4d544
AndrewLosseff <AndrewLosseff@gmail.com>
2022-10-27 16:27:14 +0300

    add problem

---
+++ src/0021-MergeTwoSortedLists/index.ts
@@ -0,0 +1,13 @@
+import { ListNode, arrayToList } from "../utils/ConvertArrayToLinkedList";
+import { convertLinkedListToArray } from "../utils/ConvertLinkedListToArray";
+
+export function mergeTwoLists(arr1, arr2): number[] {
+    // Helper function to convert arr to Linkes List
+    const list1: ListNode | null = arrayToList(arr1)
+    const list2: ListNode | null = arrayToList(arr2)
+    // Solution to the problem
+
+    
+
+    // return convertLinkedListToArray()
+};
