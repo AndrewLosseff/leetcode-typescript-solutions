066a648d07937ecbf76fb95491ab9ec379065ef8
AndrewLosseff <AndrewLosseff@gmail.com>
2022-10-28 11:41:12 +0300

    add solution

--- src/1299-ReplaceElementsWithGreatestElementOnRightSide/solutions.ts
+++ src/1299-ReplaceElementsWithGreatestElementOnRightSide/solutions.ts
@@ -1,2 +1,16 @@
 // There are some solutions for the problem
 // #1 
+export function replaceElements(arr: number[]): number[] {
+
+    const replacedArr: number[] = []
+    let maxElement = -1
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+    
+        replacedArr.unshift(maxElement)
+        maxElement = Math.max(arr[i], maxElement)
+    
+    }
+
+    return replacedArr
+};
