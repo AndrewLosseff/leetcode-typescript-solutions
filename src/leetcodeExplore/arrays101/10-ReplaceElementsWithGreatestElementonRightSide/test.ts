1fa9ff4808420b88d59e4f29c49d3601e14d13e9
AndrewLosseff <AndrewLosseff@gmail.com>
2022-12-08 15:42:39 +0200

    add replaceElements solution

---
+++ src/leetcodeExplore/arrays101/10-ReplaceElementsWithGreatestElementonRightSide/test.ts
@@ -0,0 +1,15 @@
+// To run test use this:
+// yarn jest src/leetcodeExplore/arrays101/10-ReplaceElementsWithGreatestElementonRightSide --watch
+import { replaceElements } from './index'
+
+test('Check If N and Its Double Exist', () => {
+  expect(replaceElements).toBeDefined();
+});
+
+test('nums = [17,18,5,4,6,1]', () => {
+  expect(replaceElements([17,18,5,4,6,1])).toEqual([18,6,6,6,1,-1]);
+});
+
+test('nums = [400]', () => {
+  expect(replaceElements([400])).toEqual([-1]);
+});
