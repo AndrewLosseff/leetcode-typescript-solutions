a69317491ebdf88e4ce9f8f1b4a4f3cdc92f4c80
AndrewLosseff <AndrewLosseff@gmail.com>
2022-12-14 16:30:06 +0200

    add Squares of a Sorted Array

---
+++ src/leetcodeExplore/arrays101/16-SquaresOfaSortedArray/test.ts
@@ -0,0 +1,15 @@
+// To run test use this:
+// yarn jest src/leetcodeExplore/arrays101/16-SquaresOfaSortedArray --watch
+import { sortedSquares } from './index'
+
+test('Squares of a Sorted Array', () => {
+  expect(sortedSquares).toBeDefined();
+});
+
+test('nums = [-4,-1,0,3,10]', () => {
+  expect(sortedSquares([-4,-1,0,3,10])).toEqual([0,1,9,16,100]);
+});
+
+test('nums = [-7,-3,2,3,11]', () => {
+  expect(sortedSquares([-7,-3,2,3,11])).toEqual([4,9,9,49,121]);
+});
