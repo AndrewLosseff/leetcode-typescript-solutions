99fefcc8885b026527cdd3c9322c10edd04a60e6
AndrewLosseff <AndrewLosseff@gmail.com>
2022-12-15 16:51:23 +0200

    add solution Find All Numbers Disappeared in an Array

--- src/leetcodeExplore/arrays101/15-FindAllNumbersDisappearedInAnArray/solution.ts
+++ src/leetcodeExplore/arrays101/15-FindAllNumbersDisappearedInAnArray/solution.ts
@@ -1,1 +1,11 @@
 // #1 Time: O(?) Space: O(?)
+export function findDisappearedNumbers(nums: number[]): number[] {
+    const arr = Array.from(Array(nums.length).keys(), item => item + 1)
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];    
+        arr[num - 1] = 0
+    }
+    
+    return arr.filter(n => n !== 0)
+};
