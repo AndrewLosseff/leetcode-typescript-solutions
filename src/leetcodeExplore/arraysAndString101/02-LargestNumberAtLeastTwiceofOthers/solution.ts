ae01f6a210be1515dac491a446f0469aa01a8e3a
AndrewLosseff <AndrewLosseff@gmail.com>
2022-12-21 12:20:54 +0200

    add dominantIndex solution

--- src/leetcodeExplore/arraysAndString101/02-LargestNumberAtLeastTwiceofOthers/solution.ts
+++ src/leetcodeExplore/arraysAndString101/02-LargestNumberAtLeastTwiceofOthers/solution.ts
@@ -1,5 +1,15 @@
 /*
-    Time: O(?)
-    Space: O(?)
+    Time: O(nlogn)
+    Space: O(n)
 */
+export function dominantIndex(nums: number[]): number {
+    const arr = [...nums]
+    arr.sort((a,b) => b - a )
+
+    if (arr[1] * 2 <= arr[0] ) {
+        return nums.indexOf(arr[0])
+    }
+    
+    return -1
+}
 
