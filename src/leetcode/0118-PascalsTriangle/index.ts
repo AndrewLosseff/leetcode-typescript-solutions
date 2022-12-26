7b2fe02f46a40d6073691bb35294f839dbd74c61
AndrewLosseff <AndrewLosseff@gmail.com>
2022-12-26 15:58:31 +0200

    add Pascals Triangle solution

--- src/leetcode/0118-PascalsTriangle/index.ts
+++ src/leetcode/0118-PascalsTriangle/index.ts
@@ -1,3 +1,3 @@
 export function generate(numRows: number): number[][] {
-
+   
 };
--- src/leetcode/0118-PascalsTriangle/solutions.ts
+++ src/leetcode/0118-PascalsTriangle/solutions.ts
@@ -1,2 +1,18 @@
 // There are some solutions for the problem
-// #1 
+// #1 TIME: O(N^2) SPACE: O(N^2)
+export function generate(numRows: number): number[][] {
+    const results = [[1]]
+
+    for (let i = 1; i < numRows; i++) {
+        const tempArr: number[] = [0, ...results[results.length - 1], 0]
+        const row: number[] = []
+
+        for (let j = 0; j < tempArr.length - 1; j++) { 
+            row.push(tempArr[j] + tempArr[j + 1])
+        }
+
+        results.push(row)
+    }
+
+    return results
+};
