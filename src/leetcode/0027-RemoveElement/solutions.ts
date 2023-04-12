a165e62b0fd2d948f9cac3b54c54857cb3fe3527
AndrewLosseff <AndrewLosseff@gmail.com>
2023-04-12 13:38:45 +0300

    Add optimal solution

--- src/leetcode/0027-RemoveElement/solutions.ts
+++ src/leetcode/0027-RemoveElement/solutions.ts
@@ -1,18 +1,31 @@
 // There are some solutions for the problem
-// #1 
+// #1 Time: O(n^2) Space: O(1)
 export function removeElement(nums: number[], val: number): number {
-    let count = 0
-
     for (let i = 0; i < nums.length; i++) {
         
         if(nums[i] === val) {
             nums.splice(i,1)
             i--
-        } else {
-            count++
         }
+    }
         
+    return nums.length
+};
+
+// #2 Time: O(n) Space: O(1)
+export function removeElement(nums: number[], val: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        if (nums[left] === val) {
+            nums[left] = nums[right];
+            right--;
+        }
+        else {
+            left++;
+        }
     }
 
-    return count
-};
+    return left;
+}
